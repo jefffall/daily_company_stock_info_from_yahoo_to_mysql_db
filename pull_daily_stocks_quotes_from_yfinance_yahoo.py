33d094840f9603939009619a3b862d2c3d3b4690
Jeffrey Fall <jobsjeff@gmail.com>
2021-08-10 15:54:23 -0700

    added ,Flush=True to print statements so that tail -f on hte log file from cron would show realtime

--- pull_daily_stocks_quotes_from_yfinance_yahoo.py
+++ pull_daily_stocks_quotes_from_yfinance_yahoo.py
@@ -9,7 +9,7 @@ started_at = str(datetime.datetime.now())
 parts = started_at.split(" ")
 today_date = parts[0]
 
-print ("Run started at "+str(started_at))
+print ("Run started at "+str(started_at),flush=True)
 
 def write_to_eoddata_table(these_columns):
     con = pymysql.connect( host="localhost", user="user1", passwd="user1", db="eoddata" )
@@ -38,31 +38,30 @@ def read_stocks_from_csv_download_via_yfinance_to_mysql():
             skip = False
         except:
             bad_symbol_or_bad_date = bad_symbol_or_bad_date + 1
-            print ("bad symbol: "+symbol.strip()+" or date: start="+str(today_date))
+            print ("bad symbol: "+symbol.strip()+" or date: start="+str(today_date),flush=True)
             time.sleep(1)
             skip = True
         if skip == False:
+            time.sleep (1)
             columns = "INSERT INTO stocks (symbol, date, exchange, open, low, high, close, volume) "+\
                             "VALUES ('"+symbol.strip()+"' ,'"+today_date+" 15:00:00'"+" ,'"+"NYSEorNASD"+"', "+myopen+", "+high+" ,"+low+", "+close+", "+volume+")"
             lines_processed = lines_processed + 1
-            print ("Processed: "+symbol.strip()+" --> "+str(lines_processed)+" stocks processed...")
-            print ("Date:"+str(today_date)+" Open:"+str(myopen)+" Low:"+str(low)+" High:"+str(high)+" Close:"+str(close)+" Volume:"+str(volume))
+            print ("Processed: "+symbol.strip()+" --> "+str(lines_processed)+" stocks processed...",flush=True)
+            print ("Date:"+str(today_date)+" Open:"+str(myopen)+" Low:"+str(low)+" High:"+str(high)+" Close:"+str(close)+" Volume:"+str(volume),flush=True)
             yfinance_list.append(columns)
-            time.sleep(1)
         
-    print (str(len(yfinance_list))+" symbols downloaded. Writing to mysql database now...")
-    print (" ")
-    write_to_eoddata_table(yfinance_list)
+    print (str(len(yfinance_list))+" symbols downloaded. Writing to mysql database now...",flush=True)
+    print (" ",flush=True)
+    write_to_eoddata_table(yfinance_list,flush=True)
     mycsv.close()
-    print (" ")
-    print ("Run started at "+str(started_at))
-    print ("Run finished at: ",datetime.datetime.now())
-    print ("Bad symbols or bad date count = "+str(bad_symbol_or_bad_date))
-    print (str(lines_processed)+" Equities processed...")
-    print (" ")
+    print (" ",flush=True)
+    print ("Run started at "+str(started_at),flush=True)
+    print ("Run finished at: ",datetime.datetime.now(),flush=True)
+    print ("Bad symbols or bad date count = "+str(bad_symbol_or_bad_date),flush=True)
+    print (str(lines_processed)+" Equities processed...",flush=True)
+    print (" ",flush=True)
 
 read_stocks_from_csv_download_via_yfinance_to_mysql()
 
 time.sleep(20)
 restart.restart_stocks_rising()
-
