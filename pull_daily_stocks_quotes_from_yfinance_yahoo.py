7997d20ce1f238968a3faa1578ddac895a1c486b
Jeffrey Fall <jobsjeff@gmail.com>
2021-08-10 15:19:36 -0700

    Changed daily stock pull for quotes all around and using yfinance tickerData = yf.Ticker(symbol.strip()) todayData = tickerData.history(period='1d')

--- pull_daily_stocks_quotes_from_yfinance_yahoo.py
+++ pull_daily_stocks_quotes_from_yfinance_yahoo.py
@@ -6,17 +6,8 @@ import pymysql
 import RestartStocksRising as restart
 
 started_at = str(datetime.datetime.now())
-
-def get_start_end_dates():
-    started_at = str(datetime.datetime.now())
-    parts = started_at.split(" ")
-    start_date = parts[0]
-    parts = start_date.split("-")
-    next_day_str = str(int(parts[2])+1)
-    if len(next_day_str) < 2:
-        next_day_str = "0" + next_day_str
-    next_day = parts[0]+"-"+parts[1]+"-"+next_day_str
-    return start_date.strip(), next_day.strip()
+parts = started_at.split(" ")
+today_date = parts[0]
 
 print ("Run started at "+str(started_at))
 
@@ -33,55 +24,45 @@ def read_stocks_from_csv_download_via_yfinance_to_mysql():
     lines_processed = 0
     bad_symbol_or_bad_date = 0
     mycsv = open ("eoddata_nyse_nasdaq_stock_list_cleaned.csv","r")
-    start, end = get_start_end_dates()
+    #start, end = get_start_end_dates()
     yfinance_list = []
-    mydata = None
     for symbol in mycsv:
+        tickerData = yf.Ticker(symbol.strip())
+        todayData = tickerData.history(period='1d')
         try:
-            data = yf.download(symbol, start=start, end=end)
+            myopen = str(round(float(todayData['Open'][0]),2))
+            low =  str(round(float(todayData['Low'][0]),2))
+            high =  str(round(float(todayData['High'][0]),2))
+            close =  str(round(float(todayData['Close'][0]),2))
+            volume =  str(todayData['Volume'][0])
+            skip = False
         except:
-            mydata = str(data)
-            #print (mydata)
-            print ("exiting....")
-            exit(0)
-        print ("mydata = ",mydata)
-        if mydata != None:
-            print (mydata)
-            parts = mydata.split("\n")
-            clean_string = ' '.join(parts[2].split())
-            rows = clean_string.split(" ")
-            date = rows[0]
-            myopen = str(round(float(rows[1]),2))
-            high =  str(round(float(rows[2]),2))
-            low =  str(round(float(rows[3]),2))
-            close =  str(round(float(rows[4]),2))
-            adjclose =  str(round(float(rows[5]),2))
-            volume =  str(round(float(rows[1]),6))
-            print (date, myopen,high, low, close, adjclose, volume)
-            time.sleep (1)
+            bad_symbol_or_bad_date = bad_symbol_or_bad_date + 1
+            print ("bad symbol: "+symbol.strip()+" or date: start="+str(today_date))
+            time.sleep(1)
+            skip = True
+        if skip == False:
             columns = "INSERT INTO stocks (symbol, date, exchange, open, low, high, close, volume) "+\
-                           "VALUES ('"+symbol.strip()+"' ,"+date+" ,'"+"NYSEorNASD"+"', "+myopen+", "+high+" ,"+low+", "+close+", "+volume+")"
+                            "VALUES ('"+symbol.strip()+"' ,'"+today_date+" 15:00:00'"+" ,'"+"NYSEorNASD"+"', "+myopen+", "+high+" ,"+low+", "+close+", "+volume+")"
             lines_processed = lines_processed + 1
-            print ("Processed: "+symbol.strip()+" "+str(lines_processed)+" stocks processed...")
+            print ("Processed: "+symbol.strip()+" --> "+str(lines_processed)+" stocks processed...")
+            print ("Date:"+str(today_date)+" Open:"+str(myopen)+" Low:"+str(low)+" High:"+str(high)+" Close:"+str(close)+" Volume:"+str(volume))
             yfinance_list.append(columns)
             time.sleep(1)
-        else:
-            bad_symbol_or_bad_date = bad_symbol_or_bad_date + 1
-            mydata = None
-            print ("bad symbol: "+symbol.strip()+" or date: start="+str(start)+" end="+str(end))
-            time.sleep(1)
+        
     print (str(len(yfinance_list))+" symbols downloaded. Writing to mysql database now...")
     print (" ")
     write_to_eoddata_table(yfinance_list)
     mycsv.close()
     print (" ")
     print ("Run started at "+str(started_at))
-    print ("Run finished at: ",datetime.datetime.now()) 
-    print ("Bad symbols or bad date count = "+str(bad_symbol_or_bad_date)) 
+    print ("Run finished at: ",datetime.datetime.now())
+    print ("Bad symbols or bad date count = "+str(bad_symbol_or_bad_date))
     print (str(lines_processed)+" Equities processed...")
     print (" ")
-    
+
 read_stocks_from_csv_download_via_yfinance_to_mysql()
 
 time.sleep(20)
 restart.restart_stocks_rising()
+
